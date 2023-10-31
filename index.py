5698effd4e65e4ddf6a9ff607893b88d38c2a0b1
Zeynep Aşiyan Koşun <112477149+zeynepasiyankosun@users.noreply.github.com>
2023-10-31 23:03:51 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,8 @@
+from classes import Human 
+
+#Python'da from ve import ifadeleri bir modülü veya belirli nesneleri başka bir Python betiğine veya modüle eklemek için kullanılır. 
+
+import random
+
+#"import random" ifadesi, Python'da "random" adlı bir modülü projenize eklemeyi ifade eder.
+#Python'ın standart kütüphanesinde bulunan "random" modülü, rastgele sayılar üretmek ve rastgele olaylar gerçekleştirmek için kullanılır.
